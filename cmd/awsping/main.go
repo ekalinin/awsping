8e23ca5628bd038109483074759967e2a7e70b17
Eugene Kalinin <e.v.kalinin@gmail.com>
2021-11-21 00:08:00 +0300

    Better help. Add description for all verbose levels into help

--- cmd/awsping/main.go
+++ cmd/awsping/main.go
@@ -15,7 +15,7 @@ var (
 	useHTTP     = flag.Bool("http", false, "Use http transport (default is tcp)")
 	useHTTPS    = flag.Bool("https", false, "Use https transport (default is tcp)")
 	showVer     = flag.Bool("v", false, "Show version")
-	verbose     = flag.Int("verbose", 0, "Verbosity level")
+	verbose     = flag.Int("verbose", 0, "Verbosity level (0: name-latency); 1: code-name-latency; 2: code-name-tries-avg")
 	service     = flag.String("service", "dynamodb", "AWS Service: ec2, sdb, sns, sqs, ...")
 	listRegions = flag.Bool("list-regions", false, "Show list of regions")
 )
